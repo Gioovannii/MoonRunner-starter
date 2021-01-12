d7010f7929e21550d531f866fa144e1071d44645
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-12 15:23:47 +0800

    Extended UnitSpeed with 3 class property to expressed time in minutes km or min/mile

--- MoonRunner/UnitExtensions.swift
+++ MoonRunner/UnitExtensions.swift
@@ -49,3 +49,17 @@ class UnitConverterPace: UnitConverter {
     return 1.0 / value
   }
 }
+
+extension UnitSpeed {
+  class var secondsPerMeter: UnitSpeed {
+    return UnitSpeed(symbol: "sec/m", converter: UnitConverterPace(coefficient: 1))
+  }
+  
+  class var minutesPerKilometer: UnitSpeed {
+    return UnitSpeed(symbol: "min/km", converter: UnitConverterPace(coefficient: 60.0 / 1000.0))
+  }
+  
+  class var minutesPerMile: UnitSpeed {
+    return UnitSpeed(symbol: "min/mi", converter: UnitConverterPace(coefficient: 60.0 / 1609.34))
+  }
+}
