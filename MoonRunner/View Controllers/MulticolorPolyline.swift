22e0befd42675ac22ffe83143f952ef7b5aaafa3
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-14 16:46:37 +0800

    new fil for colors

---
+++ MoonRunner/View Controllers/MulticolorPolyline.swift
@@ -0,0 +1,68 @@
+//
+/**
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import MapKit
+
+class MulticolorPolyline: MKPolyline {
+  
+  var color = UIColor.black
+  
+  private func segmentColor(speed: Double, midSpeed: Double, slowestSpeed: Double, fastestSpeed: Double) -> UIColor {
+    enum BaseColors {
+      static let r_red: CGFloat = 1
+      static let r_green: CGFloat = 20 / 255
+      static let r_blue: CGFloat = 44 / 255
+      
+      static let y_red: CGFloat = 1
+      static let y_green: CGFloat = 215 / 255
+      static let y_blue: CGFloat = 0
+      
+      static let g_red: CGFloat = 0
+      static let g_green: CGFloat = 146 / 255
+      static let g_blue: CGFloat = 78 / 255
+    }
+    let red, green, blue: CGFloat
+    
+    if speed < midSpeed {
+      let ratio = CGFloat((speed - slowestSpeed) / (midSpeed - slowestSpeed))
+      red = BaseColors.r_red + ratio * (BaseColors.y_red - BaseColors.r_red)
+      green = BaseColors.r_green + ratio * (BaseColors.y_green - BaseColors.r_green)
+      blue = BaseColors.r_blue + ratio * (BaseColors.y_blue - BaseColors.r_blue)
+    } else {
+      let ratio = CGFloat((speed - midSpeed) / (fastestSpeed - midSpeed))
+      red = BaseColors.y_red + ratio * (BaseColors.g_red - BaseColors.y_red)
+      green = BaseColors.y_green + ratio * (BaseColors.g_green - BaseColors.y_green)
+      blue = BaseColors.y_blue + ratio * (BaseColors.g_blue - BaseColors.y_blue)
+    }
+    return UIColor(red: red, green: green, blue: blue, alpha: 1)
+  }
+}
