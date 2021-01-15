bc75d6431aeb6c4ab0efa82bf51582f1d99c8554
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-01-15 15:57:03 +0800

    Set multiple polyline

--- MoonRunner/View Controllers/RunDetailsViewController.swift
+++ MoonRunner/View Controllers/RunDetailsViewController.swift
@@ -88,13 +88,38 @@ class RunDetailsViewController: UIViewController {
     return MKCoordinateRegion(center: center, span: span)
   }
   
-  private func polyLine() -> MKPolyline {
-    guard let locations = run.locations else { return MKPolyline() }
-    let coords: [CLLocationCoordinate2D] = locations.map { location in
-      let location = location as! Location
-      return CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+  private func polyLine() -> [MulticolorPolyline] {
+    
+    let locations = run.locations?.array as! [Location]
+    var coordinates: [(CLLocation, CLLocation)] = []
+    var speeds: [Double] = []
+    var minSpeed = Double.greatestFiniteMagnitude
+    var maxSpeed = 0.0
+    
+    for (first, second) in zip(locations, locations.dropFirst()) {
+      let start = CLLocation(latitude: first.latitude, longitude: first.longitude)
+      let end = CLLocation(latitude: second.latitude, longitude: second.longitude)
+      coordinates.append((start, end))
+      
+      let distance = end.distance(from: start)
+      let time = second.timestamp!.timeIntervalSince(first.timestamp! as Date)
+      let speed = time > 0 ? distance / time : 0
+      speeds.append(speed)
+      minSpeed = min(minSpeed, speed)
+      maxSpeed = max(maxSpeed, speed)
     }
-    return MKPolyline(coordinates: coords, count: coords.count)
+    
+    let midSpeed = speeds.reduce(0, +) / Double(speeds.count)
+    
+    var segments: [MulticolorPolyline] = []
+    for ((start, end), speed) in zip(coordinates, speeds) {
+      let coords = [start.coordinate, end.coordinate]
+      let segment = MulticolorPolyline(coordinates: coords, count: 2)
+      segment.color = segmentColor(speed: speed, midSpeed: midSpeed, slowestSpeed: minSpeed, fastestSpeed: maxSpeed)
+      
+      segments.append(segment)
+    }
+    return segments
   }
   
   private func loadMap() {
